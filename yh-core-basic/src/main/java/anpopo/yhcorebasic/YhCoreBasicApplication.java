03a5143d74c60db228b5476b3ce870ee1e071a9a
ansehyeong <dkstpgud@gmail.com>
2022-07-05 08:31:56 +0900

    init

---
+++ yh-core-basic/src/main/java/anpopo/yhcorebasic/YhCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package anpopo.yhcorebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YhCoreBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YhCoreBasicApplication.class, args);
+    }
+
+}
---
+++ yh-core-basic/src/test/java/anpopo/yhcorebasic/YhCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package anpopo.yhcorebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YhCoreBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
